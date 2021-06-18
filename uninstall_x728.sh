ea92d86f8e42d913fae41a7768a09a8d3e90a7cc
huangyp <harry@geekworm.com>
2021-06-18 16:49:18 +0800

    update

--- uninstall_x728.sh
+++ uninstall_x728.sh
@@ -3,5 +3,8 @@
 sudo sed -i '/x728/d' /etc/rc.local
 sudo sed -i '/x728/d' /etc/modules
 sudo sed -i '/x728/d' ~/.bashrc
-#sudo rm /home/pi/x728*.py -rf
-echo 'please remove old python file such x728xx.py on /home/pi/ fold'
+
+sudo rm /home/pi/x728*.py -rf
+sudo rm /usr/local/bin/x728softsd.sh -f
+sudo rm /etc/x728pwr.sh -f
+#echo 'please remove old python file such x728xx.py on /home/pi/ fold'
