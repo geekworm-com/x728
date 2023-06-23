4f246aeccaee37a4615a6c0f07173338f8144088
huangyp <harry@geekworm.com>
2023-06-23 12:28:11 +0800

    update

--- x728-v2.1.sh
+++ x728-v2.1.sh
@@ -159,8 +159,8 @@ while True:
         print("AC Power OK")
         GPIO.output(BUZZER_PIN, 0)
     # You can remove the following comment lines (remove the # character) if you want buzzer sounds alarm.
-    #elif i == 1:
-        #print("Power Supply A/C Lost")
+    elif i == 1:
+        print("Power Supply A/C Lost")
         #GPIO.output(BUZZER_PIN, 1)
         #time.sleep(0.1)
         #GPIO.output(BUZZER_PIN, 0)
