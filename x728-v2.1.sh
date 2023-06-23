3cbf7b3185b5fbe8184c30841cd35c041369eb8e
huangyp <harry@geekworm.com>
2023-06-23 12:29:00 +0800

    Open BUZZER out

--- x728-v2.1.sh
+++ x728-v2.1.sh
@@ -161,10 +161,10 @@ while True:
     # You can remove the following comment lines (remove the # character) if you want buzzer sounds alarm.
     elif i == 1:
         print("Power Supply A/C Lost")
-        #GPIO.output(BUZZER_PIN, 1)
-        #time.sleep(0.1)
-        #GPIO.output(BUZZER_PIN, 0)
-        #time.sleep(0.1)
+        GPIO.output(BUZZER_PIN, 1)
+        time.sleep(0.1)
+        GPIO.output(BUZZER_PIN, 0)
+        time.sleep(0.1)
     time.sleep(1)
 ' > /home/pi/x728pld.py
 #sudo chmod +x /home/pi/x728pld.py
